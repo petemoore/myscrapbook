5810fc41101ded52e71d47294b2f7a5f6e5d8488
Pete Moore <pmoore@mozilla.com>
2014-05-27 22:39:52 +0200

    fixed

--- show_staging_mapfiles.sh
+++ show_staging_mapfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh github-sync4.dmz.scl3.mozilla.com ls -l /home/pmoore/vcs_sync/build/conversion/build-*/.hg/*git-mapfile
+ssh github-sync4.dmz.scl3.mozilla.com ls -l /home/pmoore/vcs_sync/build/conversion/build-*/.hg/{added-to-git-notes,git-mapfile,delta-git-notes,published-to-mapper,delta-for-mapper}
