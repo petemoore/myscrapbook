2a87cfa5eb864b7706b7df8656124fa86848c515
Pete Moore <pmoore@mozilla.com>
2014-05-27 22:23:30 +0200

    Added script for checking integrity of mapper database

---
+++ validate_mapper_database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "${0}")"
+
+ssh github-sync4.dmz.scl3.mozilla.com ls -1 /home/pmoore/vcs_sync/build/conversion/build-*/.hg/previous-git-mapfile </dev/null | while read file
+do
+    project="$(echo "${file}" | cut -d/ -f7)"
+    scp "github-sync4.dmz.scl3.mozilla.com:${file}" "${project}.source.mapfile"
+    curl -s "https://api-pub-build.allizom.org/mapper/${project}/mapfile/full" > "${project}.mapper.mapfile"
+    md5 "${project}.source.mapfile" "${project}.mapper.mapfile"
+    diff "${project}.source.mapfile" "${project}.mapper.mapfile"
+done
