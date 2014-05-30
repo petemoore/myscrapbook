978946bd79b8a60c031d0fcc2546a0dab5eb68c5
Pete Moore <pmoore@mozilla.com>
2014-05-30 12:47:09 +0200

    stuff

--- foopy_reports/grab_foopy_data.sh
+++ foopy_reports/grab_foopy_data.sh
@@ -6,7 +6,8 @@ function test_foopy {
     echo "Testing ${foopy}..."
     cat "${ls_results}" | while read device_path
     do
-        for file in buildbot.tac twistd.log error.flg disabled.flg watcher.log install_watcher.log
+        for file in error.flg watcher.log
+#       for file in buildbot.tac twistd.log error.flg disabled.flg watcher.log install_watcher.log
 #       for file in buildbot.tac
         do
             mkdir -p "${foopy}${device_path}"
@@ -25,13 +26,16 @@ function run_command {
 }
 
 cd "$(dirname "${0}")"
-ls_results="$(mktemp -t ls.XXXXXXXXXX)"
-# for ((i=25; i<=128; i++))
-for i in 122
+
+cat ~/.ssh/known_hosts | sed '/^foopy[0-9]/d' > ~/.ssh/known_hosts2
+mv ~/.ssh/known_hosts2 ~/.ssh/known_hosts
+for ((i=25; i<=128; i++))
+# for i in 122
 # for i in 113 116 119 122 124 126
 # for ((i=102; i<=108; i++))
 do
+    ls_results="$(mktemp -t ls.XXXXXXXXXX)"
 #   printf -v j "%03d" $i
     run_command "foopy${i}" "find /builds -maxdepth 1 -mindepth 1 -name 'panda-*' -o -name 'tegra-*'" > "${ls_results}" && test_foopy "foopy${i}" "${ls_results}"
+    rm "${ls_results}"
 done
-rm "${ls_results}"
