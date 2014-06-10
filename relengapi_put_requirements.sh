9c1b39b7773daceb2e15a51baeec9bc3ac9063a3
Pete Moore <pmoore@mozilla.com>
2014-06-10 18:43:09 +0200

    Now restarts apache

--- relengapi_put_requirements.sh
+++ relengapi_put_requirements.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "${0}")"
 cat requirements_staging.txt | ssh "${USER}@relengwebadm.private.scl3.mozilla.com" 'sudo tee /data/releng-stage/src/relengapi/requirements.txt; cd /data/releng-stage/src/relengapi/; sudo ./update'
+ssh relengwebadm.private.scl3.mozilla.com sudo ssh web1.stage.releng.webapp.scl3.mozilla.com apachectl graceful
