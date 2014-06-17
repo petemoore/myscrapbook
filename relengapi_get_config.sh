6b6a6cdf1981111184562708bafcb171f3e6696c
Pete Moore <pmoore@mozilla.com>
2014-06-17 14:18:47 +0200

    stuff

--- relengapi_get_config.sh
+++ relengapi_get_config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd "$(dirname "${0}")"
 scp "${USER}@relengwebadm.private.scl3.mozilla.com:/data/releng-stage/src/relengapi/requirements.txt" requirements_staging.txt
+scp "${USER}@relengwebadm.private.scl3.mozilla.com:/data/releng-stage/src/relengapi/settings.py"      settings_staging.py
 scp "${USER}@relengwebadm.private.scl3.mozilla.com:/data/releng/src/relengapi/requirements.txt"       requirements_prod.txt
+scp "${USER}@relengwebadm.private.scl3.mozilla.com:/data/releng/src/relengapi/settings.py"            settings_prod.py
