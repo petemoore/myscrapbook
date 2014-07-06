a230649883166f45998d8400cc05e4d8a952ae36
Pete Moore <pmoore@mozilla.com>
2014-07-06 18:27:49 +0200

    Improved reliability

--- sync-vcs1-with-gd4.sh
+++ sync-vcs1-with-gd4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true
 do
-    ssh -R 12344:localhost:22 vcs2vcs@vcssync1.srv.releng.usw2.mozilla.com 'rsync -av -e "ssh -p 12344 pmoore@localhost ssh" /opt/vcs2vcs/build/ gd4:/home/pmoore/vcs_sync/build/'
+    ssh -R 13425:localhost:22 vcs2vcs@vcssync1.srv.releng.usw2.mozilla.com 'rsync -av -e "ssh -oStrictHostKeyChecking=no -p 13425 pmoore@localhost ssh" /opt/vcs2vcs/build/ gd4:/home/pmoore/vcs_sync/build/'
 done
