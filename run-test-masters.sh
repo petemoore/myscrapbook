bdfd49c9e9f025dd70bf1889bf293f8db4cd2b2f
Pete Moore <pmoore@mozilla.com>
2014-07-21 14:12:28 +0200

    Added utility script to test kim's test master changes

---
+++ run-test-masters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+cd '/builds/buildbot/pmoore/test1'
+# first clean up
+rm -rf tools
+hg clone 'https://hg.mozilla.org/build/tools'
+rm -rf buildbot-configs
+hg clone 'https://hg.mozilla.org/build/buildbot-configs'
+rm -rf buildbotcustom
+hg clone 'https://hg.mozilla.org/build/buildbotcustom'
+
+export PATH='/builds/buildbot/pmoore/test1/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin'
+export PYTHONPATH='/builds/buildbot/pmoore/test1:/builds/buildbot/pmoore/test1/tools/lib/python'
+
+cd buildbot-configs
+curl 'https://bug1030753.bugzilla.mozilla.org/attachment.cgi?id=8458253' > patch
+hg import patch
+rm patch
+./test-masters.sh
