3e3041471a6a9e4f2ad03d027faa08d14d952d94
Pete Moore <pmoore@mozilla.com>
2014-07-21 16:53:56 +0200

    fixed patching

--- run-test-masters.sh
+++ run-test-masters.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-cd "/builds/buildbot/$(whoami)/test1"
+buildbot_test_dir="/builds/buildbot/$(whoami)/test1"
+cd "${buildbot_test_dir}"
 # first clean up
 rm -rf tools
 hg clone 'https://hg.mozilla.org/build/tools'
@@ -9,12 +10,12 @@ hg clone 'https://hg.mozilla.org/build/buildbot-configs'
 rm -rf buildbotcustom
 hg clone 'https://hg.mozilla.org/build/buildbotcustom'
 
-export PATH="/builds/buildbot/$(whoami)/test1/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin"
-export PYTHONPATH="/builds/buildbot/$(whoami)/test1:/builds/buildbot/$(whoami)/test1/tools/lib/python"
+export PATH="${buildbot_test_dir}/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin"
+export PYTHONPATH="${buildbot_test_dir}:${buildbot_test_dir}/tools/lib/python"
 
-ln -f -s buildbot-configs/mozilla-tests/production_config.py localconfig.py
 cd buildbot-configs
 curl 'https://bug1030753.bugzilla.mozilla.org/attachment.cgi?id=8458253' | patch -p1 -i -
+ln -f -s "${buildbot_test_dir}/buildbot-configs/mozilla-tests/production_config.py" "${buildbot_test_dir}/localconfig.py"
 ./test-masters.sh
 cd ..
 python buildbot-configs/mozilla-tests/mobile_config.py > after
