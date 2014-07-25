ea76e0e93938814896a91105db118692f66a53fe
Pete Moore <pmoore@mozilla.com>
2014-07-25 22:38:34 +0200

    fixes

--- compare_old_vs_new_l10n_vcs_sync.sh
+++ compare_old_vs_new_l10n_vcs_sync.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 cd "$(dirname "${0}")"
-[ ! -f all_l10n_git_repos ] && curl -q 'http://git.mozilla.org/'  | sed -n 's/.*p=releases\/l10n\/\([^\/]*\)\/\([a-z]*\)\.git;a=.*/\1 \2/p' > all_l10n_git_repos
+[ ! -f all_l10n_git_repos ] && curl -q 'http://git.mozilla.org/'  | sed -n 's/.*p=releases\/l10n\/\([^\/]*\)\/\([a-z]*\)\.git;a=.*/\1 \2/p' | sort -u > all_l10n_git_repos
 
 while read locale project
 do
-    staging_url="http://github.com/petermoore/l10n-${locale}-${project}.git"
-    prod_url="http://git.mozilla.org/releases/l10n/${locale}/${project}.git"
+    staging_url="git@github.com:petermoore/l10n-${locale}-${project}.git"
+    prod_url="ssh://gitolite3@git.mozilla.org/releases/l10n/${locale}/${project}.git"
     echo "Comparing ${locale} ${project}"
     echo "==========${locale}=${project}" | sed 's/./=/g'
-    git ls-remote "${staging_url}" > staging
+    git ls-remote "${staging_url}" 2>/dev/null | grep -v 'refs/notes/commits' > staging
+    [ ! -s staging ] && echo "ERROR: Staging repo could not be queried: https://github.com/petermoore/l10n-${locale}-${project}"
     git ls-remote "${prod_url}" > prod
     diff staging prod
+    echo
 done < all_l10n_git_repos
