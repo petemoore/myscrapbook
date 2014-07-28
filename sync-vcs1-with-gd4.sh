2cdd2260b7b3eb523420a1ef26dac6ed7fd5b1e7
Pete Moore <pmoore@mozilla.com>
2014-07-28 10:02:10 +0200

    stuff

--- sync-vcs1-with-gd4.sh
+++ sync-vcs1-with-gd4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while true
 do
-    ssh -R 15436:localhost:22 vcs2vcs@vcssync1.srv.releng.usw2.mozilla.com 'rsync -av -e "ssh -oStrictHostKeyChecking=no -p 15436 pmoore@localhost ssh" /opt/vcs2vcs/build/ gd4:/home/pmoore/vcs_sync/build/'
+    ssh -R 13243:localhost:22 vcs2vcs@vcssync1.srv.releng.usw2.mozilla.com 'rsync -av -e "ssh -oStrictHostKeyChecking=no -p 13243 pmoore@localhost ssh" --delete /opt/vcs2vcs/build/ gd4:/home/pmoore/vcs_sync/build/'
 done
