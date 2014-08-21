8b250d2cd83b60708727cafb4e1ce50ce0f2b2a7
Pete Moore <pmoore@mozilla.com>
2014-08-21 15:44:56 +0200

    stuff

--- write_blog.sh
+++ write_blog.sh
@@ -2,6 +2,36 @@
 
 cd "$(dirname "${0}")"
 
-# curl -s 'https://bugzilla.mozilla.org/buglist.cgi?cmdtype=runnamed&namedcmd=Anything%20I%27ve%20worked%20on&list_id=10321014' | sed 's/[^0-9].*//' | sort -run | while read line; do curl "https://bugzilla.mozilla.org/show_bug.cgi?id=${line}" 2>/dev/null | sed -n 's/.*document\.title = "//p' | sed 's/";$//' | sed "s/\\\\//g"; done | while read Bug NUM minus rest; do echo "<p><a href=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" title=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" target=\"_blank\">Bug ${NUM} &ndash; ${rest}</a><br/></p>"; done
-# cat bugs.page | sed -n 's/.*"show_bug.cgi?id=\([0-9]*\)".*/\1/p' | head -20 | sed 's/[^0-9].*//' | sort -run | while read line; do curl "https://bugzilla.mozilla.org/show_bug.cgi?id=${line}" 2>/dev/null | sed -n 's/.*document\.title = "//p' | sed 's/";$//' | sed "s/\\\\//g"; done | while read Bug NUM minus rest; do echo "<p><a href=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" title=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" target=\"_blank\">Bug ${NUM} &ndash; ${rest}</a><br/></p>"; done
-cat bugs.list | sed 's/[^0-9].*//' | sort -run | while read line; do curl "https://bugzilla.mozilla.org/show_bug.cgi?id=${line}" 2>/dev/null | sed -n 's/.*document\.title = "//p' | sed 's/";$//' | sed "s/\\\\//g"; done | while read Bug NUM minus rest; do echo "<p><a href=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" title=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" target=\"_blank\">Bug ${NUM} &ndash; ${rest}</a><br/></p>"; done
+curl -s 'https://bugzilla.mozilla.org/page.cgi?id=user_activity.html&action=run&who=pmoore%40mozilla.com&from=2014-07-30&to=2014-08-21&sort=bug' > temp
+
+cat temp | grep '\(href="show_bug\.cgi?id=\([0-9]*\)">\|<td>(new bug)\)' | sed 's/.*href="show_bug\.cgi?id=\([0-9]*\)">.*/\1/p' | sed 's/.*(new bug).*/%/' | while read line
+do
+    if [ "${line}" = '%' ]; then
+        echo "${old_line}"
+    fi
+    old_line="${line}"
+done | sort -run > new_bugs
+cat temp | sed -n 's/.*href="show_bug\.cgi?id=\([0-9]*\)">.*/\1/p' | sort -run > all_bugs
+
+diff new_bugs all_bugs | sed -n 's/^> //p' > updated_bugs
+
+function process {
+    while read line; do curl "https://bugzilla.mozilla.org/show_bug.cgi?id=${line}" 2>/dev/null | sed -n 's/.*document\.title = "//p' | sed 's/";$//' | sed "s/\\\\//g"; done | while read Bug NUM minus rest; do echo "<li><a href=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" title=\"https://bugzilla.mozilla.org/show_bug.cgi?id=${NUM}\" target=\"_blank\">Bug ${NUM} &ndash; ${rest}</a></li>"; done
+}
+
+echo '<p><strong>Highlights from this week</strong></p>'
+echo '<ul>'
+echo '<li>xyz'
+echo '</ul>'
+echo '<p><strong>Goals for next week:</strong></p>'
+echo '<ul>'
+echo '<li>xyz'
+echo '</ul>'
+echo '<p><strong>Bugs I created this week:</strong></p>'
+echo '<ul>'
+cat new_bugs | process
+echo '</ul>'
+echo '<p><strong>Other bugs I updated this week:</strong></p>'
+echo '<ul>'
+cat updated_bugs | process
+echo '</ul>'
