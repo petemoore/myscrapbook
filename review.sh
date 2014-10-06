79aa2c23efffdb2bf24d4f03b4c1fb8998be0136
Pete Moore <pmoore@mozilla.com>
2014-10-06 09:59:43 +0200

    touched up

--- review.sh
+++ review.sh
@@ -2,3 +2,4 @@
 
 url="${1}"
 curl -L "${url}" | patch -p1 -i -
+echo "${url}" | sed -n 's/https:\/\/bug\([0-9]*\)\.bugzilla\.mozilla\.org\/attachment\.cgi?id=\([0-9]*\)/https:\/\/bugzilla.mozilla.org\/page.cgi?id=splinter.html\&bug=\1\&attachment=\2/p' | xargs open
