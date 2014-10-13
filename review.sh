6292849358cce98d3bd3046f38920e93460311c9
Pete Moore <pmoore@mozilla.com>
2014-10-13 11:17:06 +0200

    stuff

--- review.sh
+++ review.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 url="${1}"
-curl -L "${url}" | patch -p1 -i -
 echo "${url}" | sed -n 's/https:\/\/bug\([0-9]*\)\.bugzilla\.mozilla\.org\/attachment\.cgi?id=\([0-9]*\)/https:\/\/bugzilla.mozilla.org\/page.cgi?id=splinter.html\&bug=\1\&attachment=\2/p' | xargs open
+view $(curl -L "${url}" | patch -p1 -i - | sed -n 's/^patching file //p')
