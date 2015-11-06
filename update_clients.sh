9cbde3c7327cc2fcfdcd77d0b629ad03c8834de8
Pete Moore <pmoore@mozilla.com>
2015-11-06 16:26:18 +0100

    whitespace changes

--- update_clients.sh
+++ update_clients.sh
@@ -4,7 +4,7 @@ say -v boing "let's go mr driver"
 source ~/update_clients.env
 if [ -z "${TASKCLUSTER_CLIENT_ID}" ] || [ -z "${TASKCLUSTER_ACCESS_TOKEN}" ]; then
     say "No credentials for me to use with taskcluster"
-	exit 1
+    exit 1
 fi
 export DEBUG=*
 for repo in taskcluster-client-go taskcluster-client-java
@@ -26,7 +26,7 @@ do
             if git push origin master; then
                 say "New changes pushed, yahoo"
                 if [ "${repo}" == 'taskcluster-client-java' ]; then
-					version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
+                    version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
                     mvn javadoc:javadoc
                     git checkout gh-pages
                     rm -rf apidocs
@@ -38,45 +38,45 @@ do
                     else
                         say "Could not publish updated javadocs"
                     fi
-					git reset master
-					git checkout -f master
-					git clean -fd
-					if mvn deploy -P release; then
-					    say "Published java client version ${version} to maven central"
-					else
-						say "Could not publish java client version ${version} to maven central"
-					fi
-					if git tag -m "Released version ${version}" "v${version}"; then
-					    say "Tagged locally"
-						if git push origin "refs/tags/v${version}:refs/tags/v${version}"; then
-							say "Pushed tag v${version}"
-							oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
-							newLastDigit=$((oldLastDigit + 1))
-							newVersion="$(echo "${version}" | sed "s/[^\\.]*\$/${newLastDigit}/")"
-							cp pom.xml pom.xml.temp
-							cat pom.xml.temp | sed '1,/<version>/s/\(<version>\)\([0-9\.]*\)\(<\/version>\)/\1'"${newVersion}"'\3/' > pom.xml
-							rm pom.xml.temp
-							git add pom.xml
-							message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
-							git commit -m "${message}"
-							if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
-								say "Bumped version from ${version} to ${newVersion} for future release"
-							else
-								say "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
-							fi
+                    git reset master
+                    git checkout -f master
+                    git clean -fd
+                    if mvn deploy -P release; then
+                        say "Published java client version ${version} to maven central"
+                    else
+                        say "Could not publish java client version ${version} to maven central"
+                    fi
+                    if git tag -m "Released version ${version}" "v${version}"; then
+                        say "Tagged locally"
+                        if git push origin "refs/tags/v${version}:refs/tags/v${version}"; then
+                            say "Pushed tag v${version}"
+                            oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
+                            newLastDigit=$((oldLastDigit + 1))
+                            newVersion="$(echo "${version}" | sed "s/[^\\.]*\$/${newLastDigit}/")"
+                            cp pom.xml pom.xml.temp
+                            cat pom.xml.temp | sed '1,/<version>/s/\(<version>\)\([0-9\.]*\)\(<\/version>\)/\1'"${newVersion}"'\3/' > pom.xml
+                            rm pom.xml.temp
+                            git add pom.xml
+                            message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
+                            git commit -m "${message}"
+                            if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
+                                say "Bumped version from ${version} to ${newVersion} for future release"
+                            else
+                                say "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
+                            fi
 
-							if git push origin master; then
-								say "Pushed new version number to github"
-							else
-								say "Could not push new version number to github"
-							fi
-						else
-							say "Problem pushing tag v${version}"
-						fi
-					else
-						say "Could not tag java client with version ${version} locally"
-						say "Therefore can also not push tag"
-					fi
+                            if git push origin master; then
+                                say "Pushed new version number to github"
+                            else
+                                say "Could not push new version number to github"
+                            fi
+                        else
+                            say "Problem pushing tag v${version}"
+                        fi
+                    else
+                        say "Could not tag java client with version ${version} locally"
+                        say "Therefore can also not push tag"
+                    fi
                 fi
             else
                 say "New changes, but I can't push them"
