228ba8c6007254479b5560dc20997380c57ea8ae
Pete Moore <pmoore@mozilla.com>
2015-11-09 17:45:26 +0100

    update_clients.sh now understands about date changes

--- update_clients.sh
+++ update_clients.sh
@@ -11,75 +11,86 @@ for repo in taskcluster-client-go taskcluster-client-java
 do
     export GOPATH="$(mktemp -d -t update_clients.XXXXXXXXXX)"
     export PATH="${GOPATH}/bin:${PATH}"
+    export TIMESTAMP="$(date +%s)"
     rm -rf "${GOPATH}"
     go get -t "github.com/taskcluster/${repo}"
     cd "${GOPATH}/src/github.com/taskcluster/${repo}"
     # this will fail if there are changes
-    if ! ./build.sh; then
+    if ! ./build.sh -d; then
         # but is not the only reason it might fail, so let's see
         # if adding changes fixes it
         git add .
-        git commit -m "Regenerated library to pick up latest schema changes"
+        git commit -m "Temporary commit that should not get pushed to github"
         # build again to make sure all is ok since git status should report no differences this time, so exit code is reliable
         # note we only push if this second build is successful
-        if ./build.sh; then
-            if git push origin master; then
-                say "New changes pushed, yahoo"
-                if [ "${repo}" == 'taskcluster-client-java' ]; then
-                    version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
-                    mvn javadoc:javadoc
-                    git checkout gh-pages
-                    rm -rf apidocs
-                    mv target/site/apidocs .
-                    git add apidocs
-                    git commit -m "Regenerated javadocs after api update(s)"
-                    if git push origin gh-pages:gh-pages; then
-                        say "New javadocs published"
-                    else
-                        say "Could not publish updated javadocs"
-                    fi
-                    git reset master
-                    git checkout -f master
-                    git clean -fd
-                    if mvn deploy -P release; then
-                        say "Published java client version ${version} to maven central"
-                    else
-                        say "Could not publish java client version ${version} to maven central"
-                    fi
-                    if git tag -m "Released version ${version}" "v${version}"; then
-                        say "Tagged locally"
-                        if git push origin "refs/tags/v${version}:refs/tags/v${version}"; then
-                            say "Pushed tag v${version}"
-                            oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
-                            newLastDigit=$((oldLastDigit + 1))
-                            newVersion="$(echo "${version}" | sed "s/[^\\.]*\$/${newLastDigit}/")"
-                            cp pom.xml pom.xml.temp
-                            cat pom.xml.temp | sed '1,/<version>/s/\(<version>\)\([0-9\.]*\)\(<\/version>\)/\1'"${newVersion}"'\3/' > pom.xml
-                            rm pom.xml.temp
-                            git add pom.xml
-                            message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
-                            git commit -m "${message}"
-                            if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
-                                say "Bumped version from ${version} to ${newVersion} for future release"
-                            else
-                                say "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
-                            fi
+        if ./build.sh -d; then
+            # build now passes, so finally let's update timestamps by rebuilding without -d
+            say "New successful changes found, building again to pick up new date"
+            if ./build.sh; then
+                # remove last temporary commit from lines above
+                git reset 'HEAD~1'
+                git add .
+                git commit -m "Regenerated library to pick up latest schema changes"
+                if git push origin master; then
+                    say "New changes pushed, yahoo"
+                    if [ "${repo}" == 'taskcluster-client-java' ]; then
+                        version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
+                        mvn javadoc:javadoc
+                        git checkout gh-pages
+                        rm -rf apidocs
+                        mv target/site/apidocs .
+                        git add apidocs
+                        git commit -m "Regenerated javadocs after api update(s)"
+                        if git push origin gh-pages:gh-pages; then
+                            say "New javadocs published"
+                        else
+                            say "Could not publish updated javadocs"
+                        fi
+                        git reset master
+                        git checkout -f master
+                        git clean -fd
+                        if mvn deploy -P release; then
+                            say "Published java client version ${version} to maven central"
+                        else
+                            say "Could not publish java client version ${version} to maven central"
+                        fi
+                        if git tag -m "Released version ${version}" "v${version}"; then
+                            say "Tagged locally"
+                            if git push origin "refs/tags/v${version}:refs/tags/v${version}"; then
+                                say "Pushed tag v${version}"
+                                oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
+                                newLastDigit=$((oldLastDigit + 1))
+                                newVersion="$(echo "${version}" | sed "s/[^\\.]*\$/${newLastDigit}/")"
+                                cp pom.xml pom.xml.temp
+                                cat pom.xml.temp | sed '1,/<version>/s/\(<version>\)\([0-9\.]*\)\(<\/version>\)/\1'"${newVersion}"'\3/' > pom.xml
+                                rm pom.xml.temp
+                                git add pom.xml
+                                message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
+                                git commit -m "${message}"
+                                if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
+                                    say "Bumped version from ${version} to ${newVersion} for future release"
+                                else
+                                    say "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
+                                fi
 
-                            if git push origin master; then
-                                say "Pushed new version number to github"
+                                if git push origin master; then
+                                    say "Pushed new version number to github"
+                                else
+                                    say "Could not push new version number to github"
+                                fi
                             else
-                                say "Could not push new version number to github"
+                                say "Problem pushing tag v${version}"
                             fi
                         else
-                            say "Problem pushing tag v${version}"
+                            say "Could not tag java client with version ${version} locally"
+                            say "Therefore can also not push tag"
                         fi
-                    else
-                        say "Could not tag java client with version ${version} locally"
-                        say "Therefore can also not push tag"
                     fi
+                else
+                    say "New changes, but I can't push them"
                 fi
             else
-                say "New changes, but I can't push them"
+                say "Build without minus d failed, this is a serious problem"
             fi
         else
             say "Can't auto rebuild, something is seriously wrong"
