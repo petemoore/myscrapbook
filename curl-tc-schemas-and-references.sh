f0b80a5c1a6a1ca9ca22eb21530b4068c10ec594
Pete Moore <pmoore@mozilla.com>
2016-04-14 14:35:13 +0200

    Now doesn't require local installation of taskcluster-client-go but curls the model-data.txt file instead

--- curl-tc-schemas-and-references.sh
+++ curl-tc-schemas-and-references.sh
@@ -3,9 +3,17 @@
 rm -rf ~/tc-refs-and-schemas
 mkdir -p ~/tc-refs-and-schemas
 cd ~/tc-refs-and-schemas
-sed -n -e 's/.*http/http/p' "${GOPATH}/src/github.com/taskcluster/taskcluster-client-go/codegenerator/model/model-data.txt"  | grep taskcluster | grep -v "'" | sort -u | sed -n 's/^http:\/\///p' | sed 's/#$//' | while read FILE
+MODEL="$(mktemp -t "model.XXXXXXXXXX")"
+URL_LIST="$(mktemp -t "url-list.XXXXXXXXXX")"
+curl -Ls 'https://raw.githubusercontent.com/taskcluster/taskcluster-client-go/master/codegenerator/model-data.txt' > "${MODEL}"
+cat "${MODEL}" | sed -n "s/.* = '\(http:\\/\\/schemas\\.taskcluster\\.net\/.*\)'\$/\1/p" | sed 's/#.*//' >> "${URL_LIST}"
+cat "${MODEL}" | grep '^http://references\.taskcluster\.net/' >> "${URL_LIST}"
+cat "${URL_LIST}" | sort -u | while read URL
 do
+	FILE="${URL#http://}"
+    echo "${URL} =>" ~/"tc-refs-and-schemas/${FILE}"
     mkdir -p "$(dirname "${FILE}")"
-    curl -L "http://${FILE}" > "${FILE}"
+    curl -Ls "${URL}" > "${FILE}"
 done
-echo "Look in:" ~/tc-refs-and-schemas "directory"
+rm "${MODEL}"
+rm "${URL_LIST}"
