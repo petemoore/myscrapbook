c9b0a94d3460ede1b3b446f562cf5691435ab998
Pete Moore <pmoore@mozilla.com>
2016-11-16 18:48:52 +0100

    Fixed script to bootstrap worker, and made it possible to add extra steps dynamically at boot-up

--- run-generic-worker.bat
+++ run-generic-worker.bat
@@ -3,7 +3,13 @@
 :: cd to folder containing this script
 pushd %~dp0
 
-go get -u github.com/taskcluster/generic-worker
-copy /y /b %GOPATH%\bin\generic-worker.exe .\generic-worker.exe
+set GOPATH=C:\gopath
 
-.\generic-worker.exe run --configure-for-aws > .\generic-worker.log 2>&1
+go get -u github.com/taskcluster/generic-worker > .\generic-worker.log 2>&1
+copy /y /b %GOPATH%\bin\generic-worker.exe .\generic-worker.exe >> .\generic-worker.log 2>&1
+
+c:\cygwin\bin\bash.exe -c '/usr/bin/wget https://raw.githubusercontent.com/petemoore/myscrapbook/master/worker-pre-run-steps.bat' >> .\generic-worker.log 2>&1
+call .\worker-pre-run-steps.bat >> .\generic-worker.log 2>&1
+
+echo running worker >> .\generic-worker.log 2>&1
+.\generic-worker.exe run --configure-for-aws >> .\generic-worker.log 2>&1
