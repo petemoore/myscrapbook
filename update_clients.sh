b1646cd0768c164625c7c6fd60c13afff9ee52ff
Pete Moore <pmoore@mozilla.com>
2017-05-04 15:29:28 +0200

    Fixed to always use voice in say command

--- update_clients.sh
+++ update_clients.sh
@@ -3,7 +3,7 @@
 say -v boing "let's go mr driver"
 source ~/update_clients.env
 if [ -z "${TASKCLUSTER_CLIENT_ID}" ] || [ -z "${TASKCLUSTER_ACCESS_TOKEN}" ]; then
-    say "No credentials for me to use with taskcluster"
+    say -v Samantha "No credentials for me to use with taskcluster"
     exit 1
 fi
 export DEBUG=*
@@ -25,14 +25,14 @@ do
         # note we only push if this second build is successful
         if ./build.sh; then
             # build now passes, so finally let's update timestamps by rebuilding without -d
-            say "New successful changes found, building again to pick up new date"
+            say -v Samantha "New successful changes found, building again to pick up new date"
             if ./build.sh -d; then
                 # remove last temporary commit from lines above
                 git reset 'HEAD~1'
                 git add .
                 git commit -m "Regenerated library to pick up latest schema changes"
                 if git push "git@github.com:taskcluster/${repo}.git" master; then
-                    say "New changes pushed, yahoo"
+                    say -v Samantha "New changes pushed, yahoo"
                     if [ "${repo}" == 'taskcluster-client-java' ]; then
                         version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
                         mvn javadoc:javadoc
@@ -42,22 +42,22 @@ do
                         git add apidocs
                         git commit -m "Regenerated javadocs after api update(s)"
                         if git push "git@github.com:taskcluster/${repo}.git" gh-pages:gh-pages; then
-                            say "New javadocs published"
+                            say -v Samantha "New javadocs published"
                         else
-                            say "Could not publish updated javadocs"
+                            say -v Samantha "Could not publish updated javadocs"
                         fi
                         git reset master
                         git checkout -f master
                         git clean -fd
                         if mvn deploy -P release; then
-                            say "Published java client version ${version} to maven central"
+                            say -v Samantha "Published java client version ${version} to maven central"
                         else
-                            say "Could not publish java client version ${version} to maven central"
+                            say -v Samantha "Could not publish java client version ${version} to maven central"
                         fi
                         if git tag -m "Released version ${version}" "v${version}"; then
-                            say "Tagged locally"
+                            say -v Samantha "Tagged locally"
                             if git push "git@github.com:taskcluster/${repo}.git" "refs/tags/v${version}:refs/tags/v${version}"; then
-                                say "Pushed tag v${version}"
+                                say -v Samantha "Pushed tag v${version}"
                                 oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
                                 newLastDigit=$((oldLastDigit + 1))
                                 newVersion="$(echo "${version}" | sed "s/[^\\.]*\$/${newLastDigit}/")"
@@ -68,37 +68,37 @@ do
                                 message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
                                 git commit -m "${message}"
                                 if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
-                                    say "Bumped version from ${version} to ${newVersion} for future release"
+                                    say -v Samantha "Bumped version from ${version} to ${newVersion} for future release"
                                 else
-                                    say "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
+                                    say -v Samantha "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
                                 fi
 
                                 if git push "git@github.com:taskcluster/${repo}.git" master; then
-                                    say "Pushed new version number to github"
+                                    say -v Samantha "Pushed new version number to github"
                                 else
-                                    say "Could not push new version number to github"
+                                    say -v Samantha "Could not push new version number to github"
                                 fi
                             else
-                                say "Problem pushing tag v${version}"
+                                say -v Samantha "Problem pushing tag v${version}"
                             fi
                         else
-                            say "Could not tag java client with version ${version} locally"
-                            say "Therefore can also not push tag"
+                            say -v Samantha "Could not tag java client with version ${version} locally"
+                            say -v Samantha "Therefore can also not push tag"
                         fi
                     fi
                 else
-                    say "New changes, but I can't push them"
+                    say -v Samantha "New changes, but I can't push them"
                 fi
             else
-                say "Build without minus d failed, this is a serious problem"
+                say -v Samantha "Build without minus d failed, this is a serious problem"
             fi
         else
-            say "Can't auto rebuild, something is seriously wrong"
+            say -v Samantha "Can't auto rebuild, something is seriously wrong"
             git status
             git diff
         fi
     else
-        say "${repo} ok"
+        say -v Samantha "${repo} ok"
     fi
     cd
     rm -rf "${GOPATH}"
