afcbe0b2466bb841d195db32a61ab426fc62b336
Pete Moore <pmoore@mozilla.com>
2018-01-10 13:31:33 +0100

    Avoid TTY in git commands when updating taskcluster clients

--- update_clients.sh
+++ update_clients.sh
@@ -3,10 +3,11 @@ GPG_NO_TTY="$(mktemp -t gpg-no-tty.XXXXXXXXXX)"
 {
   echo '#!/bin/bash'
   echo 'gpg --no-tty "${@}"'
+  echo 'exit $?'
 } >> "${GPG_NO_TTY}"
 chmod u+x "${GPG_NO_TTY}"
 
-function git {
+function git_no_tty {
     git -c "gpg.program=${GPG_NO_TTY}" "${@}"
 }
 
@@ -29,8 +30,8 @@ do
     if ! ./build.sh; then
         # but is not the only reason it might fail, so let's see
         # if adding changes fixes it
-        git add .
-        git commit -m "Temporary commit that should not get pushed to github"
+        git_no_tty add .
+        git_no_tty commit -m "Temporary commit that should not get pushed to github"
         # build again to make sure all is ok since git status should report no differences this time, so exit code is reliable
         # note we only push if this second build is successful
         if ./build.sh; then
@@ -38,35 +39,35 @@ do
             say -v Samantha "New successful changes found, building again to pick up new date"
             if ./build.sh -d; then
                 # remove last temporary commit from lines above
-                git reset 'HEAD~1'
-                git add .
-                git commit -m "Regenerated library to pick up latest schema changes"
-                if git push "git@github.com:taskcluster/${repo}.git" master; then
+                git_no_tty reset 'HEAD~1'
+                git_no_tty add .
+                git_no_tty commit -m "Regenerated library to pick up latest schema changes"
+                if git_no_tty push "git@github.com:taskcluster/${repo}.git" master; then
                     say -v Samantha "New changes pushed, yahoo"
                     if [ "${repo}" == 'taskcluster-client-java' ]; then
                         version="$(cat pom.xml | sed -n '1,/<version>/s/.*<version>\([0-9\.]*\)<\/version>.*/\1/p')"
                         mvn javadoc:javadoc
-                        git checkout gh-pages
+                        git_no_tty checkout gh-pages
                         rm -rf apidocs
                         mv target/site/apidocs .
-                        git add apidocs
-                        git commit -m "Regenerated javadocs after api update(s)"
-                        if git push "git@github.com:taskcluster/${repo}.git" gh-pages:gh-pages; then
+                        git_no_tty add apidocs
+                        git_no_tty commit -m "Regenerated javadocs after api update(s)"
+                        if git_no_tty push "git@github.com:taskcluster/${repo}.git" gh-pages:gh-pages; then
                             say -v Samantha "New javadocs published"
                         else
                             say -v Samantha "Could not publish updated javadocs"
                         fi
-                        git reset master
-                        git checkout -f master
-                        git clean -fd
+                        git_no_tty reset master
+                        git_no_tty checkout -f master
+                        git_no_tty clean -fd
                         if mvn deploy -P release; then
                             say -v Samantha "Published java client version ${version} to maven central"
                         else
                             say -v Samantha "Could not publish java client version ${version} to maven central"
                         fi
-                        if git tag -m "Released version ${version}" "v${version}"; then
+                        if git_no_tty tag -m "Released version ${version}" "v${version}"; then
                             say -v Samantha "Tagged locally"
-                            if git push "git@github.com:taskcluster/${repo}.git" "refs/tags/v${version}:refs/tags/v${version}"; then
+                            if git_no_tty push "git@github.com:taskcluster/${repo}.git" "refs/tags/v${version}:refs/tags/v${version}"; then
                                 say -v Samantha "Pushed tag v${version}"
                                 oldLastDigit="$(echo "${version}" | sed 's/.*\.//')"
                                 newLastDigit=$((oldLastDigit + 1))
@@ -74,16 +75,16 @@ do
                                 cp pom.xml pom.xml.temp
                                 cat pom.xml.temp | sed '1,/<version>/s/\(<version>\)\([0-9\.]*\)\(<\/version>\)/\1'"${newVersion}"'\3/' > pom.xml
                                 rm pom.xml.temp
-                                git add pom.xml
+                                git_no_tty add pom.xml
                                 message="Bumped version from ${version} to ${newVersion} in preparation for a future release"
-                                git commit -m "${message}"
+                                git_no_tty commit -m "${message}"
                                 if [ "$(git log --grep "${message}" | wc -l)" -gt 0 ]; then
                                     say -v Samantha "Bumped version from ${version} to ${newVersion} for future release"
                                 else
                                     say -v Samantha "git commit seemed to fail for the version bump from ${version} to ${newVersion}"
                                 fi
 
-                                if git push "git@github.com:taskcluster/${repo}.git" master; then
+                                if git_no_tty push "git@github.com:taskcluster/${repo}.git" master; then
                                     say -v Samantha "Pushed new version number to github"
                                 else
                                     say -v Samantha "Could not push new version number to github"
@@ -104,8 +105,8 @@ do
             fi
         else
             say -v Samantha "Can't auto rebuild, something is seriously wrong"
-            git status
-            git diff
+            git_no_tty status
+            git_no_tty diff
         fi
     else
         say -v Samantha "${repo} ok"
