c7a3052438497b7be3f41b79356dbe6e58f678b7
Pete Moore <pmoore@mozilla.com>
2018-05-08 14:16:37 +0200

    Added comments

--- sync-worker-type-definitions.sh
+++ sync-worker-type-definitions.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -xv
+
+# This is a utility script that I run in a cron job, locally.  It syncs the
+# live production worker type definitions with a local git repository. This
+# allows me to crudely track changes to worker type definitions.  I don't
+# publish the repository, because it contains secrets, so if you also want to
+# track history, you could run this too. My local repository is located at
+# ~/worker_type_definitions (hard coded in this script).
+
+# My cron job looks like this:
+#  00,05,10,15,20,25,30,35,40,45,50,55 * * * * /Users/pmoore/git/mozilla/sync-worker-type-definitions.sh > ~/sync-worker-type-definitions.log 2>&1
+
+# This muckery is to prevent gpg asking for a passphrase when making git commits
 GPG_NO_TTY="$(mktemp -t gpg-no-tty.XXXXXXXXXX)"
 {
   echo '#!/bin/bash'
@@ -11,7 +23,7 @@ function git_no_tty {
     git -c "gpg.program=${GPG_NO_TTY}" "${@}"
 }
 
-# This sets TASKCLUSTER_CLIENT_ID, TASKCLUSTER_ACCESS_TOKEN and PATH.
+# The sourced file exports TASKCLUSTER_CLIENT_ID, TASKCLUSTER_ACCESS_TOKEN and PATH.
 # I personally use this client, fwiw:
 # https://auth.taskcluster.net/v1/clients/mozilla-auth0%2Fad%7CMozilla-LDAP%7Cpmoore%2Ffetch-worker-type-definitions
 source ~/sync-worker-type-definitions.env
