63f93447a01a436b6905a5c0da9c41e4d4d7d4f4
Pete Moore <pmoore@mozilla.com>
2018-05-14 15:52:03 +0200

    say what you see

--- sync-worker-type-definitions.sh
+++ sync-worker-type-definitions.sh
@@ -28,18 +28,19 @@ function git_no_tty {
 # https://auth.taskcluster.net/v1/clients/mozilla-auth0%2Fad%7CMozilla-LDAP%7Cpmoore%2Ffetch-worker-type-definitions
 source ~/sync-worker-type-definitions.env
 
-# say -v Daniel "let's sync those definitions"
+say -v Daniel "let's sync those definitions"
 
 cd ~/worker_type_definitions
 git_no_tty clean -fdx
 git_no_tty reset --hard
 cd ..
 # See https://github.com/taskcluster/generic-worker/blob/master/worker_types/all-worker-types/main.go
+date
 all-worker-types
+date
 cd worker_type_definitions
 git_no_tty add .
 git_no_tty commit -m "$(git status --porcelain)"
-
 rm "${GPG_NO_TTY}"
 
-# say -v Daniel "definitions have all been sunk, sir!"
+say -v Daniel "definitions have all been sunk, sir!"
