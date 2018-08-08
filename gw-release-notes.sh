8d8acf60a4335f68d4ea9d1a2757ac076ae2acb4
Pete Moore <pmoore@mozilla.com>
2018-08-08 11:29:17 +0200

    updates

---
+++ gw-release-notes.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function process {
+  while read line; do
+    curl "https://bugzilla.mozilla.org/show_bug.cgi?id=${line}" 2>/dev/null | sed -n 's/.*BUGZILLA.bug_title = '\''//p' | sed 's/'\'';$//' | sed "s/\\\\//g"
+  done | while read bugnumber title; do
+  echo "* [Bug ${bugnumber} ${title}](https://bugzil.la/${bugnumber})"
+  done
+}
+
+# GW_CHECKOUT="$(mktemp -d -t gw-checkout.XXXXXXXXXX)"
+GW_CHECKOUT=/var/folders/v9/mll6p_rj5h94dt_m5m8j0f9c0000gn/T/gw-checkout.XXXXXXXXXX.qPjQYsRh
+cd "${GW_CHECKOUT}"
+# git clone https://github.com/taskcluster/generic-worker
+cd generic-worker
+git tag -l | grep '^v' > ../tags
+git tag -l | sed -n 's/^v/v /p' | sed 's/alpha/ alpha /g' | gsed 's/\<[0-9]\>/0&/g' | sed 's/ //g' > ../padded-tags
+tag_count=$(cat ../tags | wc -l)
+for ((i=1;i<=$tag_count;i++));
+do
+  tag=$(sed -n "${i}p" ../tags)
+  padded_tag=$(sed -n "${i}p" ../padded-tags)
+  echo ${padded_tag}'}' ${tag}
+done | sort -u > ../sorted-tags
+
+while read x tag; do
+  if [ -n "${previous_tag}" ]; then
+    git log ${previous_tag}..${tag} | sed -n 's/.*[Bb][Uu][Gg] *\([1-9][0-9][0-9][0-9][0-9][0-9]*\).*/\1/p' | sort -u > ../${tag}
+	bug_count=$(cat ../${tag} | wc -l)
+    if [ ${bug_count} -gt 0 ]; then
+      {
+      echo
+      title="In ${tag} since ${previous_tag}"
+      echo "${title}"
+      echo "${title//?/=}"
+	  echo
+      cat ../${tag} | process
+      } > ../${tag}-text
+    fi
+  fi
+  previous_tag=${tag}
+done < ../sorted-tags
+
+tail -r ../sorted-tags | while read x tag; do
+  cat ../${tag}-text 2>/dev/null
+done
--- sync-worker-type-definitions.sh
+++ sync-worker-type-definitions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash -xve
 
 # This is a utility script that I run in a cron job, locally.  It syncs the
 # live production worker type definitions with a local git repository. This
@@ -28,7 +28,7 @@ function git_no_tty {
 # https://auth.taskcluster.net/v1/clients/mozilla-auth0%2Fad%7CMozilla-LDAP%7Cpmoore%2Ffetch-worker-type-definitions
 source ~/sync-worker-type-definitions.env
 
-say -v Daniel "let's sync those definitions"
+say -v Daniel "syncing"
 
 cd ~/worker_type_definitions
 git_no_tty clean -fdx
@@ -36,11 +36,17 @@ git_no_tty reset --hard
 cd ..
 # See https://github.com/taskcluster/generic-worker/blob/master/worker_types/all-worker-types/main.go
 date
-all-worker-types
+if ! all-worker-types; then
+  say -v Daniel "Something went wrong updating worker types"
+  exit 64
+fi
 date
 cd worker_type_definitions
-git_no_tty add .
-git_no_tty commit -m "$(git status --porcelain)"
+if test $(git_no_tty status --porcelain | wc -l) != 0; then
+  say -v Daniel "Worker type changes have been found"
+  git_no_tty add .
+  git_no_tty commit -m "$(git status --porcelain)"
+fi
 rm "${GPG_NO_TTY}"
 
-say -v Daniel "definitions have all been sunk, sir!"
+say -v Daniel "sunk!"
