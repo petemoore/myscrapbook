5e3692004311507499117fc8e95000fade86974a
Pete Moore <pmoore@mozilla.com>
2019-05-23 10:56:43 +0200

    Corrected spoken text in sync-mozilla-history.sh

--- sync-mozilla-history.sh
+++ sync-mozilla-history.sh
@@ -8,27 +8,27 @@
 # My cron job looks like this:
 #  00,05,10,15,20,25,30,35,40,45,50,55 * * * * /Users/pmoore/git/mozilla/sync-mozilla-history.sh > ~/sync-mozilla-history.log 2>&1
 
+# Note, the 'say' command is part of macOS. I prefer my computer to audibly say
+# what it is doing, and if there is a problem, so that I do not need to
+# routinely check logs. I just hear about problems (literally) if they occur.
+
 # This sourced file exports TASKCLUSTER_CLIENT_ID, TASKCLUSTER_ACCESS_TOKEN,
 # TASKCLUSTER_ROOT_URL and PATH. My client has `secrets:get:*` (required).
 source ~/sync-mozilla-history.env
-
-# say -v Daniel "syncing"
 
 export GOPATH=~/mozilla-history-gopath
 rm -rf "${GOPATH}"
 go get -u github.com/taskcluster/mozilla-history
 cd "${GOPATH}/src/github.com/taskcluster/mozilla-history"
-date
 if ! mozilla-history; then
-  say -v Daniel "Something went wrong updating worker types"
+  say -v Daniel "Something went wrong running the mozilla-history command."
   exit 64
 fi
-date
 if test $(git status --porcelain | wc -l) != 0; then
-  say -v Daniel "Worker type changes have been found"
+  say -v Daniel "Taskcluster entity changes have been found."
   git add .
   git -c "commit.gpgsign=false" commit -m "$(git status --porcelain | sed 's/\\342\\230\\205/*/g' | sed 's/\\342\\201\\204/\//g')"
   git push origin master
 fi
 
-say -v Daniel "sunk!"
+say -v Daniel "Sunk!"
