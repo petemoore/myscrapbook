8f5277ba8f2b9f9b2c2dcee656bb319e4d69a8cd
Pete Moore <pmoore@mozilla.com>
2023-02-25 10:25:18 +0100

    wip

--- rebuildkernel.sh
+++ rebuildkernel.sh
@@ -18,18 +18,21 @@ git checkout rpi-5.15.y-debug-pcie-usb
 git config pull.rebase true
 git pull pete rpi-5.15.y-debug-pcie-usb
 git pull work-laptop rpi-5.15.y-debug-pcie-usb
-KERNEL=kernel8
+export KERNEL=kernel8 # online it suggests not to export, but i don't see how it would reach make subprocess if we don't
 make bcm2711_defconfig
 sed -i 's/^\(CONFIG_LOCALVERSION=.*\)"/\1-pmoore"/' .config
 sed -i 's/-pmoore-pmoore/-pmoore/' .config
 sed -i 's/^# CONFIG_WERROR is not set/CONFIG_WERROR=y/' .config
-sed -i 's/^\(CONFIG_LOG_BUF_SHIFT=\).*/\118/' .config
+# sed -i 's/^\(CONFIG_LOG_BUF_SHIFT=\).*/\118/' .config # do not need to change default as we update /boot/cmdline.txt later
 make -j4 Image.gz modules dtbs
 sudo make modules_install
 sudo cp arch/arm64/boot/dts/broadcom/*.dtb /boot/
 sudo cp arch/arm64/boot/dts/overlays/*.dtb* /boot/overlays/
 sudo cp arch/arm64/boot/dts/overlays/README /boot/overlays/
 sudo cp arch/arm64/boot/Image.gz /boot/$KERNEL.img
+
+##### TODO: increase kernel ring buffer size in /boot/cmdline.txt with e.g. log_buf_len=4M or log-buf-len=4M
+
 echo "Kernel rebuilt"
 sleep 3
 sudo reboot
